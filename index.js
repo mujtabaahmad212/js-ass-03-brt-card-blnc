a38881e880066f08acdec402a9a425fb22581e74
mujtabaahmad212 <mujtabaahmad4200@gmail.com>
2024-05-28 08:47:53 +0500

    BRT card balance check

---
+++ index.js
@@ -0,0 +1,14 @@
+//  BRT card balance check 
+
+
+var yourbalance = +prompt( "enter your card currant balance")
+var balance = 50;
+
+if(balance <= yourbalance){
+    alert ('za warza ' )
+}
+
+if(balance > yourbalance){
+    alert ('pise nishta warke' )
+}
+
